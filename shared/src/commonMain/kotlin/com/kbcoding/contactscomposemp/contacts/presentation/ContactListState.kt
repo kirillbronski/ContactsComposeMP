6b0b637a92167a8b647d123f4b4ac66012ab6b03
Kirill <kirillbronski@gmail.com>
2023-07-16 21:36:12 +0300

    implemented ContactListState

---
+++ shared/src/commonMain/kotlin/com/kbcoding/contactscomposemp/contacts/presentation/ContactListState.kt
@@ -0,0 +1,15 @@
+package com.kbcoding.contactscomposemp.contacts.presentation
+
+import com.kbcoding.contactscomposemp.contacts.domain.model.Contact
+
+data class ContactListState(
+    val contacts: List<Contact> = emptyList(),
+    val recentlyAddedContacts: List<Contact> = emptyList(),
+    val selectedContact: Contact? = null,
+    val isAddContactSheetOpen: Boolean = false,
+    val isSelectedContactSheetOpen: Boolean = false,
+    val firstNameError: String? = null,
+    val lastNameError: String? = null,
+    val emailError: String? = null,
+    val phoneError: String? = null,
+)
